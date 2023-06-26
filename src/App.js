a68f8a65abf1b23ec761a4fb8642d5196ccae8bd
digitalera <sandervisser0407@gmail.com>
2023-06-26 08:18:41 -0700

    Some change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
 import Footer from './Containers/footer';
 import Header from './Containers/header';
+import ContactUs from './Containers/contactus';
+import CustomerBase from './Containers/customerbase';
+import Engagements from './Containers/engagements';
+import IoT from './Containers/iot';
+import LogoText from './Containers/logotext';
+import LoyalityFeature from './Containers/loyalityfeature';
+import MProductlines from './Containers/mproductlines';
+import OFeatures from './Containers/ofeatures';
 
 function App() {
   return (
