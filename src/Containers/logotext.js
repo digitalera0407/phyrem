bf002b90513ff1250b9d7dbf1d6bb87737f659fc
digitalera <sandervisser0407@gmail.com>
2023-06-26 10:39:29 -0700

    Some change

--- src/Containers/logotext.js
+++ src/Containers/logotext.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function LogoText() {
-    
+    <div class="timer section-spacing">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-10 col-md-offset-1 text-center"> 
+                <img src="../img/phyrem-meaning.png" data-at2x="../img/phyrem-meaning.png" alt="PHYREM Meaning" className="phyrem-meaning img-responsive" />
+            </div>
+            </div>
+        </div>
+    </div>
 }
