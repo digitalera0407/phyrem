dd77e59c09d3553dc00336953bbf29fb683eec5b
Olaf-712 <cergeicergio82@gmail.com>
2023-06-27 02:31:05 +0200

    Complete Components

--- src/Containers/loyalityfeature.js
+++ src/Containers/loyalityfeature.js
@@ -1,5 +1,43 @@
 import React from 'react'
 
 export default function LoyalityFeature() {
-    
+    return(
+    <div class="timer section-spacing">
+        <div class="container">
+            <div class="beta_main_title">
+                <span class="beta-head"> Available Now!!!</span>
+            </div>
+            <h2 class="text-center v-loyalty"><span>v</span>-Loyalty Feature</h2>
+            <div class="row">
+                <div class="col-md-12">
+                    <img src="https://www.phyrem.com/public/beta/img/v-Loyalty Slider.png" alt="v-Loyalty Slider" class="img-responsive"></img>
+
+                    <h4 class="text-center" style={{margin: "50px", width: "85%"}}><span class="blue-big-text">Digital</span> Customer <span class="blue-big-text">Loyalty</span> Programs are the best way for Businesses to Maintain, Reward and Engage with Customers.</h4>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="slide-left feature text-center">
+                        <img src="https://www.phyrem.com/public/beta/img/maintain-icon.png" alt="MAINTAIN" class="feature-icon"></img>
+                        <h3 class="feature-list-text">MAINTAIN</h3>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="slide-top feature text-center">
+                        <img src="https://www.phyrem.com/public/beta/img/reward-icon.png" alt="REWARD" class="feature-icon"></img>
+                        <h3 class="feature-list-text">REWARD</h3>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="slide-right feature text-center">
+                        <img src="https://www.phyrem.com/public/beta/img/engage-icon.png" alt="ENGAGE" class="feature-icon"></img>
+                        <h3 class="feature-list-text">ENGAGE</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    );
 }
